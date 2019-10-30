48556d9bbc46d0466f7b3ddbd0ebb3ccf259b835
eflj2005 <eflj2005@gmail.com>
2019-10-29 20:00:18 -0500

    ok

--- src/app/usuarios/login/login.component.ts
+++ src/app/usuarios/login/login.component.ts
@@ -10,15 +10,38 @@ export class LoginComponent implements OnInit {
   usuario:string= null;
   clave:string= null;
 
-  error:any = null;
+      /*
+      Modo de inicio
+        1 = Login normal
+        2 = Nuevo Administrador
+        */
+  modo:number=null;
 
-  estado:number = null;
+  procesando:boolean=null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
     this.usuario="";
     this.clave="";
+    this.procesando=false;
+
+    this.ValidarAdministrador();
   }
 
+  ngOnInit() {
+  }
+
+  ValidarAdministrador(){
+    this.modo = 1;
+  }
+
+  ValidarLogin(){
+    alert("envio formulario Login");
+    this.procesando=true;
+    
+  }
+
+  ActivarRegitroAdministrador(){
+    alert("Activar Nuevo Administrador");
+    
+  }  
 }
