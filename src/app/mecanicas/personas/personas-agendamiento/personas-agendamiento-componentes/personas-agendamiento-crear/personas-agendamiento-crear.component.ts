2bc68ba5ede2b0a5705041bc3e6cd84c1691a757
JuanBustosS2 <lljuancarloslls2@hotmail.com>
2019-11-21 11:14:21 -0500

    ok

--- src/app/mecanicas/personas/personas-agendamiento/personas-agendamiento-componentes/personas-agendamiento-crear/personas-agendamiento-crear.component.ts
+++ src/app/mecanicas/personas/personas-agendamiento/personas-agendamiento-componentes/personas-agendamiento-crear/personas-agendamiento-crear.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-personas-agendamiento-crear',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonasAgendamientoCrearComponent implements OnInit {
 
-  constructor() { }
 
+
+    seleccion: any  ;
+    FechaInicio 
+  
+  constructor() {
+    // this.dateFormatormat(this.now, "dddd, mmmm dS, yyyy");
+    this.FechaInicio= formatDate(new Date(), 'yyyy-MM-dd', 'en');
+    console.log(this.FechaInicio)
+   }
+  
+   searchObjectPersonas: any ={
+    IdPersona:"",
+    Nombre:"",
+    Programa:"",
+    Cedula:"",
+    Id:"",
+    FechaActualizacion:"",
+
+  };
+
+  Personas: Array<Object> = [{
+    IdPersona:"1",
+    Nombre:"Juan Carlos Bustos Tovio ",
+    Programa:"Ingenieria de sistemas",
+    Cedula:"1007405687",
+    Id:"12345678",
+    FechaActualizacion:"12-12-2019",
+  },
+  {
+    IdPersona:"1",
+    Nombre:"Juan Diego Moreno Marroquin ",
+    Programa:"Ingenieria de sistemas",
+    Cedula:"1007405687",
+    Id:"12345678",
+    FechaActualizacion:"12-12-2019",
+  }
+];
+
+  Seleccion(){
+    console.log(this.seleccion)
+  }
   ngOnInit() {
+
   }
 
 }
