cc01ea04835cc291d59a004f4aa95d2d75b2633c
jaunsdw <31634597+jaunsdw@users.noreply.github.com>
2019-11-29 04:53:42 -0500

    JD

--- src/app/mecanicas/dashboard/dashboard-componentes/dashboard-componentes.module.ts
+++ src/app/mecanicas/dashboard/dashboard-componentes/dashboard-componentes.module.ts
@@ -4,16 +4,20 @@ import {RouterModule} from '@angular/router';
 import { DashboardEncabezadoComponent } from './dashboard-encabezado/dashboard-encabezado.component';
 import { DashboardMenuComponent } from './dashboard-menu/dashboard-menu.component';
 import { DashboardWorkspaceComponent } from './dashboard-workspace/dashboard-workspace.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { DashboardEstadisticasComponent } from './dashboard-estadisticas/dashboard-estadisticas.component';
+import { ChartsModule } from 'ng2-charts';
 
 
 
 
 @NgModule({
-  declarations: [DashboardEncabezadoComponent, DashboardMenuComponent, DashboardWorkspaceComponent],
+  declarations: [DashboardEncabezadoComponent, DashboardMenuComponent, DashboardWorkspaceComponent, EstadisticasComponent, DashboardEstadisticasComponent],
   imports: [
     CommonModule,
-    RouterModule
-  ],exports:[DashboardEncabezadoComponent,DashboardMenuComponent,DashboardWorkspaceComponent]
+    RouterModule,
+    ChartsModule
+  ],exports:[DashboardEncabezadoComponent,DashboardMenuComponent,DashboardWorkspaceComponent,  DashboardEstadisticasComponent]
 })
 export class DashboardComponentesModule { 
 
---
+++ src/app/mecanicas/dashboard/dashboard-componentes/dashboard-estadisticas/dashboard-estadisticas.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions ,  ChartType} from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
+
+
+@Component({
+  selector: 'app-dashboard-estadisticas',
+  templateUrl: './dashboard-estadisticas.component.html',
+  styleUrls: ['./dashboard-estadisticas.component.css']
+})
+export class DashboardEstadisticasComponent implements OnInit {
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'top',
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    }
+  };
+  public pieChartLabels: Label[] = [['Plataforma'], ['Encuesta'], 'Eventos'];
+  public pieChartData: number[] = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+    },
+  ];
+
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Sede A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Sede B' },
+    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Sede C'}
+  ];
+  public lineChartLabels: Label[] = ['Enero', 'Febreno', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+  public lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
