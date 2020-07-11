7cfaa8f67b5391b114a79ee50ddbffe4a0cc8354
jaunsdw <31634597+jaunsdw@users.noreply.github.com>
2020-07-10 23:12:44 -0500

    JD

--- src/app/mecanicas/personas/personas-agendamiento/personas-agendamiento-componentes/personas-agendamiento-crear/personas-agendamiento-crear.component.ts
+++ src/app/mecanicas/personas/personas-agendamiento/personas-agendamiento-componentes/personas-agendamiento-crear/personas-agendamiento-crear.component.ts
@@ -410,35 +410,29 @@ export class PersonasAgendamientoCrearComponent implements OnInit {
     }
     this.seleccionarTodos.nuevosAgendados = false;
     this.AplicarFiltros(3);
-    
+
   }
 
   AgregarPersonas(){
     if (this.registrosAgendados.length == 0){
-
       for (let i = 0; i < this.registrosPersonasTemp.length; i++) {
-
         if (this.registrosPersonasTemp[i].seleccionado){
           let clonPersona = Object.assign({}, this.registrosPersonasTemp[i]);
           clonPersona.seleccionado = false;
           this.registrosAgendados.push(clonPersona);
         }
-        
       }
 
     } else {
      
       for (let i = 0; i < this.registrosPersonasTemp.length; i++) {
-        
         let index = this.registrosAgendados.findIndex(element => element.documento == this.registrosPersonasTemp[i].documento);
-        if(index == -1){
+        if(index == -1 && this.registrosPersonasTemp[i].seleccionado == true){
           let clonPersona = Object.assign({}, this.registrosPersonasTemp[i]);
           clonPersona.seleccionado = false;
           this.registrosAgendados.push(clonPersona);
         }
-
       }
-
     }
 
     this.sedeid = -1;
@@ -500,7 +494,9 @@ export class PersonasAgendamientoCrearComponent implements OnInit {
                     this.controladorAsignaciones.Guardar().subscribe(
                       (respuestaAsignaciones : RespuestaInterface) => {
                         if (respuestaAsignaciones.codigo == 200) {
-                          console.log("listo")
+                          console.log("listo");
+                          alert("Agenda guardada");
+                          this.Limpiar();
                         } else {
                           alert("Error al guardar Asignaciones");
                         }
